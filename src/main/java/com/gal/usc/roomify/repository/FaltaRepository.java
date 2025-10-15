564974a09f128727a8eeb495a91c840db1100022
Xenxo314 <126247000+Xenxo314@users.noreply.github.com>
2025-10-15 10:18:07 +0200

    Interfaz FaltaRepository

---
+++ src/main/java/com/gal/usc/roomify/repository/FaltaRepository.java
@@ -0,0 +1,13 @@
+package com.gal.usc.roomify.repository;
+
+import com.gal.usc.roomify.model.Falta;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FaltaRepository extends MongoRepository <@NonNull Falta, @NonNull String> {
+}
