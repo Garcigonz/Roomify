ff25950e0cfb4ed0c2feb6ea2f813537ef3f2316
Garcigonz <ivanivan.ingagl@gmail.com>
2025-11-05 11:21:20 +0100

    Merge

--- src/main/java/com/gal/usc/roomify/repository/UsuarioRepository.java
+++ src/main/java/com/gal/usc/roomify/repository/UsuarioRepository.java
@@ -9,10 +9,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends MongoRepository<@NonNull Usuario, @NonNull String> {
     Page<Usuario> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
     Page<Usuario> findByRol(String rol, Pageable pageable);
     Page<Usuario> findByNombreContainingIgnoreCaseAndRol(String nombre, String rol, Pageable pageable);
+    Optional<@NonNull Usuario> findById(String id);
 }
