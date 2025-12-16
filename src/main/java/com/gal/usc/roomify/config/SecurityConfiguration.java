73954392c2e1d5155fe976ba039594be360d9a08
Garcigonz <ivanivan.ingagl@gmail.com>
2025-12-16 18:28:42 +0100

    SecurityConf para permitir Scalar a cualquiera

--- src/main/java/com/gal/usc/roomify/config/SecurityConfiguration.java
+++ src/main/java/com/gal/usc/roomify/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
                                 // rutas de autenticación
                                 .requestMatchers("/auth/**").permitAll()
                                 // rutas de documentación (Scalar)
-                                .requestMatchers("/scalar/**", "/v3/api-docs/**", "/docs/**").permitAll()
+                                .requestMatchers("/scalar", "/scalar.html", "/v3/api-docs/**").permitAll()
                                 // el resto requiere login
                                 .anyRequest().authenticated()
                 )
